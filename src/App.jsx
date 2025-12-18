d4234f9e6155aa1e3dd19ab1761e053c7c6f1629
Dadu Khan <photoshopt65@gmail.com>
2025-12-18 08:46:59 -0800

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import About from './pages/About';
+import Profile from './pages/Profile';
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-3xl text-red-300 font-bold underline'>duniya</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profile' element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
