8983729516bab6f0135fd8a533f089aa3c1cf727
Dadu Khan <photoshopt65@gmail.com>
2025-12-19 16:42:25 -0800

    created user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose, { Schema, Types } from "mongoose";
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+})
+
+const User = mongoose.model("User", userSchema);
+
+export default  User;
