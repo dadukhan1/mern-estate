339825e67d25d3644be28f3480d51b3171565b54
Dadu Khan <photoshopt65@gmail.com>
2025-12-21 00:48:48 -0800

    complete profile page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Profile() {
+  const { currentUser } = useSelector(state => state.user);
   return (
-    <div>Profile</div>
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <form className='flex flex-col gap-4'>
+        {console.log(currentUser)}
+
+        <img src={currentUser.avatar} alt="profile" className='rounded-full h-24 w-24 object-cover hover:cursor-pointer self-center mt-2' />
+        <input type='text' id='username' placeholder='username' className='border p-3 rounded-lg' />
+        <input type='email' id='email' placeholder='email' className='border p-3 rounded-lg' />
+        <input type='password' id='password' placeholder='password' className='border p-3 rounded-lg' />
+        <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span className='text-red-700 cursor-pointer'>Sign Out</span>
+      </div>
+    </div>
   )
 }
 
