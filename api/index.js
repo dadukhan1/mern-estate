026a795df99ef67760f86cfbd9e1f3c8a6d1e5d9
Dadu Khan <photoshopt65@gmail.com>
2025-12-25 14:13:39 +0500

    added create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
 
 app.use((err, req, res, next) => {
