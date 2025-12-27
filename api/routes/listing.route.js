50d806fbb811ea07526eb32dd5dcad0be0b73d2a
Dadu Khan <photoshopt65@gmail.com>
2025-12-27 16:45:38 +0500

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -5,6 +5,7 @@ import {
   deleteListing,
   updateListing,
   getListing,
+  getListings,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { upload } from "../utils/multer.js";
@@ -21,5 +22,6 @@ router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
 router.get("/getListing/:id", getListing);
+router.get("/get", getListings);
 
 export default router;
