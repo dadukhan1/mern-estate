4f96e4e42666518c2e5cca3385978bedc08e1000
Dadu Khan <photoshopt65@gmail.com>
2025-12-29 16:20:05 +0500

    cookie correction udpate

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -36,7 +36,7 @@ export const signin = async (req, res, next) => {
 
         const { password: pass, ...rest } = validUser._doc;
 
-        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+        res.cookie('access_token', token, { httpOnly: true,secure: true, sameSite: 'none' }).status(200).json(rest);
 
     } catch (error) {
         next(error);
@@ -50,7 +50,7 @@ export const google = async (req, res, next) => {
         if (user) {
             const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = user._doc;
-            res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+            res.cookie('access_token', token, { httpOnly: true, secure: true, sameSite: 'none' }).status(200).json(rest);
         } else {
             const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
 
@@ -59,7 +59,7 @@ export const google = async (req, res, next) => {
             await newUser.save();
             const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = newUser._doc;
-            res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+            res.cookie('access_token', token, { httpOnly: true,secure: true, sameSite: 'none' }).status(200).json(rest);
         }
 
     } catch (error) {
